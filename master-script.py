e11308448f20088336d2d51221aae67eaacfcd66
Kate L <kate@mynameiskate.com>
2015-06-09 17:11:28 -0700

    Update master-script.py
    Adding copyright

--- master-script.py
+++ master-script.py
@@ -347,6 +347,22 @@ if keepIntTables == False:
 # note: settings for this section are in update_settings.ini
 # it requires the requests and requests-master directories
 # this section came from here: http://blogs.esri.com/esri/arcgis/2014/01/24/updating-your-hosted-feature-service-for-10-2/
+
+# Copyright 2013 Esri
+# portions copyright 2015 Kate Leroux
+
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+
+#       http://www.apache.org/licenses/LICENSE-2.0
+
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
     
 def updateAGOL():
     # START OF IMPORTED SCRIPT
